860e5abec504184689a5780b59704e8e7bb98381
rifkimarthadinata <62433657+rifkimarthadinata@users.noreply.github.com>
2020-04-15 11:40:00 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -4,4 +4,13 @@ public class OperatorBinary {
     String nama = "Muhammad Zurqoni Sihab";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
