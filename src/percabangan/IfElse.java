1cdf02a4505ea819c70a2676475e8939d353d820
rifkimarthadinata <62433657+rifkimarthadinata@users.noreply.github.com>
2020-04-15 12:14:02 +0800

    percabangan kurangi jajan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,16 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
 
         if(uangDiDompet<totalBelanja) {
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsulan = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsulan : "+angsulan);
         }else{
-            System.out.println("Uang cukup, selamat menikmati");
+            System.out.println("Uang pas.. uhuyyyy...");
         }
     }
 }
